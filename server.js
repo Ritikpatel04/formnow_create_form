dd518e25152dba7637874821014453ed1b10e4fe
ritik patel <ritikpatel201819@gmail.com>
2021-08-10 19:08:22 +0530

    minor changes

---
+++ server.js
@@ -0,0 +1,15 @@
+const firebase= require('./firebase/serviceAccountkey.json');
+const express = require('express');
+const bodyParser= require('body-parser');
+const createForm = require('./src/data/createForm');
+const authMiddleware = require('./src/auth/authMiddleware');
+const app = express();
+app.use(bodyParser.json());
+app.use('*',authMiddleware);
+
+
+app.post('/create-form', createForm);
+
+app.listen(3001 , ()=>{
+    console.log('listning at port 3001')
+});
